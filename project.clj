448a1e5ef99f9f0b83519865be36357868d6a52b
Rob Culliton (*) <rculliton@GR-J18ZMD6Nmbp.local>
2021-02-11 10:26:26 -0500

    working, if arbitrary figwheel main build

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://github.com/paraseba/faker"
   :license {:name "Eclipse Public License 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.773"]
+                 [com.bhauman/figwheel-main "0.2.12"]])
